e160dec45b21fbdc311c6e91f6bd0969f8f9812a
Saranettle <sara.nettle@gmail.com>
2025-02-04 21:57:41 -0800

    added server directory, installed dependencies

--- sunscribe-app/src/pages/prev_entries.js
+++ sunscribe-app/src/pages/prev_entries.js
@@ -1,4 +1,6 @@
 import { React } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // Importing components for the application
 import Menu from '../components/menu';
